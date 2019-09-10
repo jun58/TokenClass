30a353795cecd065f65a166b58f1893458f47e06
Harry <1416127787@qq.com>
2019-09-10 16:47:32 +0800

    Updates

---
+++ precache-manifest.222b10442a505eebf74d1386ced1eaeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5797533bf476a4d6add6cfca9825f39e",
+    "url": "/TokenClass/index.html"
+  },
+  {
+    "revision": "c034dd1c7cfd43ecab3d",
+    "url": "/TokenClass/static/css/2.84fba315.chunk.css"
+  },
+  {
+    "revision": "4656ca01abb7d8347810",
+    "url": "/TokenClass/static/css/main.ad741286.chunk.css"
+  },
+  {
+    "revision": "c034dd1c7cfd43ecab3d",
+    "url": "/TokenClass/static/js/2.d05362ca.chunk.js"
+  },
+  {
+    "revision": "4656ca01abb7d8347810",
+    "url": "/TokenClass/static/js/main.760ac623.chunk.js"
+  },
+  {
+    "revision": "67a7a6436006aa893ed6",
+    "url": "/TokenClass/static/js/runtime~main.7c9b0f0c.js"
+  },
+  {
+    "revision": "ca781b6edbce3e06344b4f44747fc4c5",
+    "url": "/TokenClass/static/media/head_bg.ca781b6e.png"
+  }
+]);
